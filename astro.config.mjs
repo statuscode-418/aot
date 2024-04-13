532d004793bc39766b388d1ed2c5b0767c70acf0
Skythrill256 <anjishnuganguly773@gmail.com>
2024-04-14 01:21:33 +0530

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react({
+    include: ['**/react/*'],
+    experimentalReactChildren: true
+  }), tailwind(
+    configFile: './custom-config.mjs',
+  )]
+});
