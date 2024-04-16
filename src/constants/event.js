9fe9d7115e887219fd601e58feba113eea1ed699
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-16 23:23:22 +0530

    raw json data updated

---
+++ src/constants/event.js
@@ -0,0 +1,93 @@
+const events = [
+  {
+    "TechFesta": {
+      "event_link": "https://www.techfesta.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Annual Techfest in Academy of Technology"
+    }
+  },
+  {
+    "C3IT": {
+      "event_link": "https://www.c3it.com/",
+      "event_date": "2018-06-09",
+      "event_title": "First Annual C3IT Conference in Academy of Technology"
+    }
+  },
+  {
+    "Symposium": {
+      "event_link": "https://www.symposium.com/",
+      "event_date": "2018-06-09",
+      "event_title": "One day offline conference in Academy of Technology"
+    }
+  },
+  {
+    "TechTalks": {
+      "event_link": "https://www.techtalks.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Monthly Tech Talks at Academy of Technology"
+    }
+  },
+  {
+    "CodeCamp": {
+      "event_link": "https://www.codecamp.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Annual CodeCamp Event at Academy of Technology"
+    }
+  },
+  {
+    "InnovateCon": {
+      "event_link": "https://www.innovatecon.com/",
+      "event_date": "2018-06-09",
+      "event_title": "InnovateCon: Innovation Conference at Academy of Technology"
+    }
+  },
+  {
+    "DevDay": {
+      "event_link": "https://www.devday.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Developer Day: A day-long event for developers at Academy of Technology"
+    }
+  },
+  {
+    "AIsummit": {
+      "event_link": "https://www.aisummit.com/",
+      "event_date": "2018-06-09",
+      "event_title": "AI Summit: Advancements in Artificial Intelligence at Academy of Technology"
+    }
+  },
+  {
+    "Hackathon": {
+      "event_link": "https://www.hackathon.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Annual Hackathon at Academy of Technology"
+    }
+  },
+  {
+    "DataForum": {
+      "event_link": "https://www.dataforum.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Data Forum: Data Science and Analytics Conference at Academy of Technology"
+    }
+  },
+  {
+    "TechExpo": {
+      "event_link": "https://www.techexpo.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Tech Expo: Exhibition of Latest Technologies at Academy of Technology"
+    }
+  },
+  {
+    "IoTsummit": {
+      "event_link": "https://www.iotsummit.com/",
+      "event_date": "2018-06-09",
+      "event_title": "IoT Summit: Internet of Things Conference at Academy of Technology"
+    }
+  },
+  {
+    "StartupFair": {
+      "event_link": "https://www.startupfair.com/",
+      "event_date": "2018-06-09",
+      "event_title": "Startup Fair: Showcase of Entrepreneurial Ventures at Academy of Technology"
+    }
+  }
+];
---
+++ src/constants/marqee.js
@@ -0,0 +1,26 @@
+const marqee= [
+  {
+    "Techquisitive": {
+      "title": "Techquisitive",
+      "link": "https://techquisitive.com/"
+    }
+  },
+  {
+    "WIE": {
+      "title": "Women in Space Engineering",
+      "link": "https://wie.engr.scu.edu/"
+    }
+  },
+  {
+    "Symposium": {
+      "title": "Symposium",
+      "link": "https://wie.engr.scu.edu/symposium/"
+    }
+  },
+  {
+    "DevWrap": {
+      "title": "DevWrap",
+      "link": "https://wie2021.devpost.com/"
+    }
+  }
+];
