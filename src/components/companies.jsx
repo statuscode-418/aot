0675401803cd430fc0a8e8340c54bd48713c4331
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-21 23:22:32 +0530

    company div added

---
+++ src/components/companies.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {links} from '../constants/links'
+import '../../public/styles/animations.css'
+import { companies } from '../constants/companies';
+import '../../public/styles/bg_transparent.css'
+const Companies = () => {
+  return (
+    <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 w-full">
+      <ul className="flex justify-between items-center ">
+        {companies.map((company) => (
+          <li key={company.id}>
+            <a href={company.href} className="text-xl text-zinc-900 hover:text-zinc-500">
+              <img src={company.img_link} alt="Company" className='blend-multiply'/>
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Companies;
+
---
+++ src/constants/companies.js
@@ -0,0 +1,6 @@
+export const companies = [
+    { "img_link": "../public/assets/tcs.png", "href": "https://www.tcs.com/" },
+    { "img_link": "../public/assets/google.png", "href": "https://www.google.co.in/" },
+    { "img_link": "../public/assets/cognizant.png", "href": "https://www.cognizant.com/in/en" },
+    { "img_link": "../public/assets/amazon.png", "href": "https://www.amazon.in/" },
+]
