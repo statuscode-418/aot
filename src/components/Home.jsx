c7da5d295c93d01087ba01cee5963adfdfd3ad27
Skythrill256 <anjishnuganguly773@gmail.com>
2024-04-21 19:01:01 +0530

    changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Navbar from './navbar.jsx';
-import MarqueeAnouncement from './marqueeAnnounement.jsx';
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <div className='py-5 bg-aot-bone'>
-        <MarqueeAnouncement announcements={['Welcome to Astro', 'Astro is a new static site builder', 'Astro is built with React']} />
+      <div className='py-5 '>
       </div>
     </div>
   )
