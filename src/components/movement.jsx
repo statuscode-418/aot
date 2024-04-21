f7e904e6e03213012398da31b2353618dd9f1ffa
Skythrill256 <anjishnuganguly773@gmail.com>
2024-04-21 18:51:03 +0530

    changes

--- src/components/movement.jsx
+++ src/components/movement.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {links} from '../constants/links'
 import '../../public/styles/animations.css'
-
 const Movement = () => {
   return (
     <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 w-full">
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,25 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			keyframes: {
-				marqee: {
-					'0%': { transform: 'translateX(100%)' },
-					'100%': { transform: 'translateX(-100%)' },
-				},
-			},
-			animation: {
-				marqee: 'marqee 5s linear infinite',
-			},
-			colors: {
-				'aot-indigo': '#0F2167',
-				'aot-blue': '#3559E0',
-				'aot-blue-light': '#4CB9E7',
-				'aot-yellow': '#FFD700',
-				'aot-bone': '#FFECD6'
-			}
-		},
-	},
-	plugins: [],
-}
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        move: { // Define your custom keyframe for the 'move' animation
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        marquee: 'marquee 5s linear infinite',
+        move: 'move 30s linear infinite', 
+      },
+      colors: {
+        'aot-indigo': '#0F2167',
+        'aot-blue': '#3559E0',
+        'aot-blue-light': '#4CB9E7',
+        'aot-yellow': '#FFD700',
+        'aot-bone': '#FFECD6',
+      },
+    },
+  },
+  plugins: [],
+};
+
