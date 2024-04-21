597b1272ab186387d63a6f74707571351f681b21
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2024-04-21 16:01:47 +0530

    Added marqee animation; Added color pallet

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ export default defineConfig({
   integrations: [react({
     include: ['**/react/*'],
     experimentalReactChildren: true
-  }), tailwind({ config: './tailwind.config.mjs' })]
+  }), tailwind({
+    config: './tailwind.config.mjs'
+  })]
 });
