7d03cf00e5c872b6a5f913b3adc51d55fd9786ea
Skythrill256 <anjishnuganguly773@gmail.com>
2024-04-22 21:13:22 +0530

    changes

--- src/components/movement.jsx
+++ src/components/movement.jsx
@@ -3,7 +3,7 @@ import {links} from '../constants/links'
 import '../../public/styles/animations.css'
 const Movement = () => {
   return (
-    <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 w-full">
+    <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 w-full overflow-hidden">
       <ul className="flex justify-between items-center anime ">
         {links.map((link) => (
           <li key={link.id}>
