852e15de4aca7333ea158b0503f97d5a4b975297
Skythrill256 <anjishnuganguly773@gmail.com>
2024-04-22 22:18:36 +0530

    first commit

--- src/components/Cardwrapper.jsx
+++ src/components/Cardwrapper.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import Card from '../components/card'; // Ensure correct path to Card component
-import { card } from '../constants/card'; // Importing 'card' from 'constants/card'
+import Card from '../components/card';
+import { card } from '../constants/card';
 
 const CardWrapper = () => {
   return (
     <>
-      {card.map((item) => ( // Changed 'content' to 'card'
+      {card.map((item) => (
         <Card
           key={item.id}
           image={item.image}
           title={item.title}
-         info ={item.text} // Changed from 'info' to 'text'
-          buttonLink={item.link} // Changed from 'buttonLink' to 'link'
+          info={item.text}
+          buttonLink={item.link}
         />
       ))}
     </>
