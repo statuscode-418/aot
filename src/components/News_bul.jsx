bc473d1b158090c3e05b857ecceacd2c78c7411b
Ritam Majumder <ritmmajumder0@gmail.com>
2024-04-25 01:32:28 +0530

    news

---
+++ src/components/News_bul.jsx
@@ -0,0 +1,79 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+const News_bul = () => {
+    const news=[
+        {
+            id:1,
+            Title:"Techfiesta 2k24",
+            Author:"Dilip Kumar Maity",
+            img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+        },
+        {
+            id:2,
+            Title:"Techfiesta 2k24",
+            Author:"Dilip Kumar Maity",
+            img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+        },
+        {
+            id:3,
+            Title:"Techfiesta 2k24",
+            Author:"Dilip Kumar Maity",
+            img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+        },
+        {
+            id:4,
+            Title:"Techfiesta 2k24",
+            Author:"Dilip Kumar Maity",
+            img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+        },
+        {
+            id:5,
+            Title:"Techfiesta 2k24",
+            Author:"Dilip Kumar Maity",
+            img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+        },
+        {
+            id:6,
+            Title:"Techfiesta 2k24",
+            Author:"Dilip Kumar Maity",
+            img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+        }
+        
+    ]
+  return (
+    <div className='p-8'>
+      <div className="all_news_container flex flexwrap gap-6 items-center justify-center">
+      {news.map((card, id) => (
+    <div
+      key={id} className='p-4 border border-[3px] border-black rounded-[20px] w-[550px]'
+      >
+        <div className="image-container relative w-[500px]">
+    <img src={card.img} alt="" className='rounded-[20px]' />
+    <div className="overlay absolute left-[10px] top-[180px] text-white font-bold text-[25px]">
+        <p className="  ">{card.Title}</p>
+        
+            {card.Author}
+    </div>
+</div>
+        <div className="names text-[25px] pt-4 flex items-center justify-center">
+            {card.details}
+            
+        </div>
+
+    </div>
+  ))}
+      </div>
+    </div>
+  )
+}
+
+export default News_bul
---
+++ src/constants/news.js
@@ -0,0 +1,9 @@
+export const News = [
+    {
+      "image": "https://images.unsplash.com/photo-1713714614660-18a216d92281?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHx8",
+      "title": "Smart India Hackathon",
+      "text": "Smart India Hackathon is a nationwide initiative to provide students a platform to solve some of pressing problems we face in our daily lives, and thus inculcate a culture of product innovation and a mindset of problem solving. Academy of Technology proudly hosts the Grand Finale of Smart India Hackathon 2021.",
+      "link": "https://www.sih.gov.in/"
+    },
+  ]
+  
