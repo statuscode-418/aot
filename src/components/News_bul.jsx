5eabdd8646a1139c042e66740235dcda9bb3ff25
Ritam Majumder <ritmmajumder0@gmail.com>
2024-04-25 01:54:10 +0530

    news done

--- src/components/News_bul.jsx
+++ src/components/News_bul.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react';
-
-// Import Swiper styles
 import 'swiper/css';
 const News_bul = () => {
     const news=[
@@ -10,49 +8,65 @@ const News_bul = () => {
             Title:"Techfiesta 2k24",
             Author:"Dilip Kumar Maity",
             img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
-            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all.",
+            link:"ja khusi bosia dibi"
         },
         {
             id:2,
             Title:"Techfiesta 2k24",
             Author:"Dilip Kumar Maity",
             img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
-            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all.",
+            link:"ja khusi bosia dibi"
         },
         {
             id:3,
             Title:"Techfiesta 2k24",
             Author:"Dilip Kumar Maity",
             img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
-            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all.",
+            link:"ja khusi bosia dibi"
         },
         {
             id:4,
             Title:"Techfiesta 2k24",
             Author:"Dilip Kumar Maity",
             img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
-            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all.",
+            link:"ja khusi bosia dibi"
         },
         {
             id:5,
             Title:"Techfiesta 2k24",
             Author:"Dilip Kumar Maity",
             img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
-            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all.",
+            link:"ja khusi bosia dibi"
         },
         {
             id:6,
             Title:"Techfiesta 2k24",
             Author:"Dilip Kumar Maity",
             img:"https://www.mywbut.com/logo_pic/65ae1b28e983c01c875bb596dbd81547.jpg",
-            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all."
+            details:"All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all. Nabajit Kaushik Rohan Anjishnu Ritam all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all.",
+            link:"ja khusi bosia dibi"
         }
         
     ]
   return (
     <div className='p-8'>
+        <div className="title text-[35px] font-bold p-4">News Bulletin</div>
+        
+
       <div className="all_news_container flex flexwrap gap-6 items-center justify-center">
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
       {news.map((card, id) => (
+        <SwiperSlide>
     <div
       key={id} className='p-4 border border-[3px] border-black rounded-[20px] w-[550px]'
       >
@@ -64,13 +78,16 @@ const News_bul = () => {
             {card.Author}
     </div>
 </div>
-        <div className="names text-[25px] pt-4 flex items-center justify-center">
+        <div className="names text-[25px] pt-4 flex items-center justify-center flex-col">
             {card.details}
+            <a href={card.link}>...see more </a>
             
         </div>
 
     </div>
+    </SwiperSlide>
   ))}
+   </Swiper>
       </div>
     </div>
   )
