16fb0c839daa346b0da573a3f1dc5f0cce4bbced
Skythrill256 <anjishnuganguly773@gmail.com>
2024-04-24 11:52:25 +0530

    changes

--- src/components/Cardwrapper.jsx
+++ src/components/Cardwrapper.jsx
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 import Card from '../components/card';
 import { card } from '../constants/card';
 
 const CardWrapper = () => {
+  const [currentCards, setCurrentCards] = useState([]);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const startIndex = Math.floor(Math.random() * (card.length - 3));
+      const newCards = card.slice(startIndex, startIndex + 3);
+      
+      // Add an animation for card change
+      setCurrentCards((prevCards) => {
+        return prevCards.length === 0 ? newCards : [...prevCards.slice(1), newCards[2]];
+      });
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const cardVariants = {
+    initial: { opacity: 0, x: -100 },
+    animate: { opacity: 1, x: 0, transition: { duration: 0.5 } },
+    exit: { opacity: 0, x: 100, transition: { duration: 0.5 } }
+  };
+
   return (
-    <>
-      {card.map((item) => (
-        <Card
-          key={item.id}
-          image={item.image}
-          title={item.title}
-          info={item.text}
-          buttonLink={item.link}
-        />
-      ))}
-    </>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' , marginRight: '2 rem' }}>
+      <AnimatePresence>
+        {currentCards.map((item, index) => (
+          <motion.div
+            key={item.id}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            variants={cardVariants}
+            style={{ marginRight: index !== currentCards.length - 1 ? '2rem' : 0 }}
+          >
+            <Card
+              key={item.id}
+              image={item.image}
+              title={item.title}
+              info={item.text}
+              buttonLink={item.link}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    </div>
   );
 };
 
--- src/components/companies.jsx
+++ src/components/companies.jsx
@@ -4,8 +4,8 @@ import { companies } from '../constants/companies';
 import '../../public/styles/bg_transparent.css'
 const Companies = () => {
   return (
-    <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 w-full overflow-hidden ">
-      <ul className="flex justify-between items-center ">
+    <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 w-full overflow-hidden mix-blend-multiply ">
+      <ul className="flex justify-between items-center anime ">
         {companies.map((company) => (
           <li key={company.id} >
             <a href={company.href} target="_blank" rel="noopener noreferrer" className="text-xl text-zinc-900 hover:text-zinc-500">
