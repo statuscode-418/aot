09e40e50c5d55d05090174d3f1df38fa2708c6f7
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-24 16:52:01 +0530

    trash

--- src/components/companies.jsx
+++ src/components/companies.jsx
@@ -4,7 +4,8 @@ import { companies } from '../constants/companies';
 import '../../public/styles/bg_transparent.css'
 const Companies = () => {
   return (
-
+    <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 sm:p-5 w-full overflow-hidden ">
+      <ul className="flex justify-between items-center overflow-x-auto sm:overflow-visible">
         {companies.map((company) => (
           <li key={company.id} className="mr-4 sm:mr-4 lg:mr-0">
             <a href={company.href} target="_blank" rel="noopener noreferrer" className="text-xl text-zinc-900 hover:text-zinc-500">
