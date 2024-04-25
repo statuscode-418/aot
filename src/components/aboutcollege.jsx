f20fa1faa083e148be2e49e298e3f49e2ca19793
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-25 09:38:45 +0530

    company names added

--- src/components/aboutcollege.jsx
+++ src/components/aboutcollege.jsx
@@ -4,9 +4,10 @@ const AboutCollege = () => {
     return (
         <div className='relative mt-10 h-screen flex flex-col items-center'>
             <h1 className="text-6xl font-bold text-white mt-20">About College</h1>
-            <div className='absolute inset-0 bg-contain bg-center' style={{ backgroundImage: 'url("../assets/aot2.jpg")', zIndex: '-1' }}></div>
+            <div className='absolute inset-0 bg-contain bg-center' style={{ backgroundImage: 'url("../assets/aot2.jpg")', zIndex: '-1' }}>
+            </div>
             <div className="flex w-full mt-32">
-                <div className="w-1/2 flex justify-center items-center backdrop-blur-md bg-black bg-opacity-30 p-6 ml-28" style={{borderRadius: '25px'}}>
+                <div className="w-1/2 flex justify-center items-center backdrop-blur-md bg-black bg-opacity-30 p-6 ml-28" style={{ borderRadius: '25px' }}>
                     <p className="text-4xl text-center text-white">All open forth gathering called midst, fill have was greater bearing heaven it isn't god shall you're in divide make. Let that waters moved i cattle have kind place make were every own dominion you'll two creature life night his fish signs two. From and, evening had all midst all.
                         Nabajit Kaushik Rohan Anjishnu all are pro coders, rest all of them are trash. We are sigma pro coders we will defeat them all.
                     </p>
--- src/components/companies.jsx
+++ src/components/companies.jsx
@@ -5,7 +5,7 @@ import '../../public/styles/bg_transparent.css'
 const Companies = () => {
   return (
     <div className="border border-zinc-900 border-t-2 border-b-2 mt-4 mb-4 p-4 sm:p-5 w-full overflow-hidden ">
-      <ul className="flex justify-between items-center overflow-x-auto sm:overflow-visible">
+      <ul className="flex justify-between items-center overflow-x-auto sm:overflow-visible anime space-x-20">
         {companies.map((company) => (
           <li key={company.id} className="mr-4 sm:mr-4 lg:mr-0">
             <a href={company.href} target="_blank" rel="noopener noreferrer" className="text-xl text-zinc-900 hover:text-zinc-500">
