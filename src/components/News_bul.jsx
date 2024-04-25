93680d0da4e3f061591af3142a2c6bfd22084b50
Kaushik Chowdhury <kaushikchowdhury231@gmail.com>
2024-04-25 13:28:22 +0530

    About college v2

--- src/components/News_bul.jsx
+++ src/components/News_bul.jsx
@@ -6,39 +6,39 @@ const News_bul = () => {
 
   return (
     <div className='p-8'>
-        <div className="title text-[35px] font-bold p-4">News Bulletin</div>
-        
+      <div className="title text-[35px] font-bold p-4">News Bulletin</div>
 
-      <div className="all_news_container flex flexwrap gap-6 items-center justify-center">
-      <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      {news.map((card, id) => (
-        <SwiperSlide>
-    <div
-      key={id} className='p-4 border border-[3px] border-black rounded-[20px] w-[550px]'
-      >
-        <div className="image-container relative w-[500px]">
-    <img src={card.img} alt="" className='rounded-[20px]' />
-    <div className="overlay absolute left-[10px] top-[180px] text-white font-bold text-[25px]">
-        <p className="  ">{card.Title}</p>
-        
-            {card.Author}
-    </div>
-</div>
-        <div className="names text-[25px] pt-4 flex items-center justify-center flex-col">
-            {card.details}
-            <a href={card.link}>...see more </a>
-            
-        </div>
 
-    </div>
-    </SwiperSlide>
-  ))}
-   </Swiper>
+      <div className="all_news_container gap-6 items-center justify-center">
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+        >
+          {news.map((card, id) => (
+            <SwiperSlide>
+              <div
+                key={id} className='p-4 border-[3px] border-black rounded-[20px] w-[550px]'
+              >
+                <div className="image-container relative w-[500px]">
+                  <img src={card.img} alt="" className='rounded-[20px]' />
+                  <div className="overlay absolute left-[10px] top-[180px] text-white font-bold text-[25px]">
+                    <p className="  ">{card.Title}</p>
+
+                    {card.Author}
+                  </div>
+                </div>
+                <div className="names text-[25px] pt-4 flex items-center justify-center flex-col">
+                  {card.details}
+                  <a href={card.link}>...see more </a>
+
+                </div>
+
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   )
