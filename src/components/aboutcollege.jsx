6841cf459edfa5e9ad70f5a1de0254a7d281960e
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-25 16:24:16 +0530

    trash

--- src/components/aboutcollege.jsx
+++ src/components/aboutcollege.jsx
@@ -8,8 +8,8 @@ const AboutCollege = () => {
             </div>
             <div className="flex w-full mt-32">
                 <div className="w-1/2 flex justify-center items-center backdrop-blur-md bg-black bg-opacity-30 p-6 ml-28" style={{ borderRadius: '25px' }}>
-                    <p className="text-2xl text-center text-white">Academy of Technology (AOT), located amidst the beautiful surroundings of Adisaptagram, has been a beacon of educational excellence since its founding in 2003. More than just an engineering college, AOT is a symbol of ingenuity and dedication, weaving a lovely mix of technology and ethics.
-                        Founded by the visionary "Ananda Educational Development and Charitable Organisation" (AEDCO), AOT has evolved over 18 years, constantly reinventing itself to embrace the boundless horizons of engineering and technology. Our journey is not merely about imparting knowledge; it's a transformative experience that encourages every student to think differently and explore new possibilities.
+
+                    <p className="text-4xl lg:text-8xl text-center text-white">
                     </p>
                 </div>
                 <div className="w-1/2 flex justify-center items-center">
