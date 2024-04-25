5b2cd3ce25b5445ecc4a59ec85655208333828c7
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-25 17:17:57 +0530

    infinite animation added in companies name

--- src/components/News_bul.jsx
+++ src/components/News_bul.jsx
@@ -9,31 +9,31 @@ const News_bul = () => {
       <div className="title text-[35px] font-bold p-4">News Bulletin</div>
 
 
-      <div className="all_news_container gap-6 items-center justify-center">
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={3}
-          onSlideChange={() => console.log('slide change')}
-          onSwiper={(swiper) => console.log(swiper)}
-        >
-          {news.map((card, id) => (
-            <SwiperSlide>
-              <div
-                key={id} className='p-4 border-[3px] border-black rounded-[20px] w-[550px]'
-              >
-                <div className="image-container relative w-[500px]">
-                  <img src={card.img} alt="" className='rounded-[20px]' />
-                  <div className="overlay absolute left-[10px] top-[180px] text-white font-bold text-[25px]">
-                    <p className="  ">{card.Title}</p>
-
-                    {card.Author}
-                  </div>
-                </div>
-                <div className="names text-[25px] pt-4 flex items-center justify-center flex-col">
-                  {card.details}
-                  <a href={card.link}>...see more </a>
-
-                </div>
+      <div className="all_news_container flex flexwrap gap-6 items-center justify-center">
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      {news.map((card, id) => (
+        <SwiperSlide>
+    <div
+      key={id} className='p-4  border-[3px] border-black rounded-[20px] w-[550px]'
+      >
+        <div className="image-container relative w-[500px]">
+    <img src={card.img} alt="" className='rounded-[20px]' />
+    <div className="overlay absolute left-[10px] top-[180px] text-white font-bold text-[25px]">
+        <p className="  ">{card.Title}</p>
+        
+            {card.Author}
+    </div>
+</div>
+        <div className="names text-[25px] pt-4 flex items-center justify-center flex-col">
+            {card.details}
+            <a href={card.link}>...see more </a>
+            
+        </div>
 
               </div>
             </SwiperSlide>
--- src/components/companies.jsx
+++ src/components/companies.jsx
@@ -2,21 +2,31 @@ import React from 'react';
 import '../../public/styles/animations.css'
 import { companies } from '../constants/companies';
 import '../../public/styles/bg_transparent.css'
+
 const Companies = () => {
   return (
     <div className="border border-zinc-900 border-t-2 border-b-2 mt-2 mb-4 p-4 sm:p-3 w-full overflow-hidden ">
-      <ul className="flex justify-between items-center overflow-x-auto sm:overflow-visible anime space-x-5">
-        {companies.map((company) => (
-          <li key={company.id} className="mr-4 sm:mr-4 lg:mr-0">
-            <a href={company.href} target="_blank" rel="noopener noreferrer" className="text-xl text-zinc-900 hover:text-zinc-500">
-              <img src={company.img_source} alt="Company" className='w-full sm:w-64 h-auto' />
-            </a>
-          </li>
-        ))}
-      </ul>
+      <div className="marquee">
+        <ul className="marquee-content">
+          {companies.map((company, index) => (
+            <li key={index} className="mr4 sm:mr-4 lg:mr-0">
+              <a href={company.href} target="_blank" rel="noopener noreferrer" className="text-xl text-zinc-900 hover:text-zinc-500">
+                <img src={company.img_source} alt="Company" />
+              </a>
+            </li>
+          ))}
+          {/* Duplicate the logos */}
+          {companies.map((company, index) => (
+            <li key={index + companies.length} className="mr4 sm:mr-4 lg:mr-0">
+              <a href={company.href} target="_blank" rel="noopener noreferrer" className="text-xl text-zinc-900 hover:text-zinc-500">
+                <img src={company.img_source} alt="Company" className='w-full sm:w-64 h-auto' />
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
 
 export default Companies;
-
