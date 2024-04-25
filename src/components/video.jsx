e888d4eb353f68527ed123d2e01a68680bdb7bd3
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-25 22:55:43 +0530

    video dimentions reduced

--- src/components/video.jsx
+++ src/components/video.jsx
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 import videoSrc from '../../assets/aotvideo.webm';
 
-const video = () => {
+const Video = () => {
   return (
-    <div className=" relative h-screen">
-        <video src={videoSrc} autoPlay loop muted className="w-full h-3/4 object-cover absolute inset-0 z-0" />
-      </div>
-
-  )
+    <div className="relative h-screen" style={{ height: '75vh' }}>
+      <video src={videoSrc} autoPlay loop muted className="w-full h-full object-cover absolute inset-0 z-0" />
+    </div>
+  );
 }
 
-export default video
+export default Video;
