863d4d87010554f8539c742f40f8cb7ce0961fe3
NabajitBhadury <nabajitbhadury@gmail.com>
2024-04-30 20:28:29 +0530

    trash

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -70,11 +70,11 @@ const Counter = () => {
     <div className="relative flex flex-col items-center justify-center h-[600px] bg-cover bg-bottom bg-[url('/assets/aot3.png')]">
       <div className="absolute inset-0 bg-black opacity-40"></div>
       <div
-        className="absolute top-1/2 left-1/2 sm:top-1/2 sm:left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row justify-center items-center space-y-8 lg:space-y-0 lg:space-x-12"
+        className="absolute top-1/2 left-1/2 sm:top-1/2 sm:left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row justify-center items-center space-y-8 lg:space-y-0 lg:space-x-8"
         ref={counterRef}
       >
         {counter.map((counter, key) => (
-          <div className="text-white flex items-center" key={counter.id}>
+          <div className="text-white flex items-center" key={counter.id} >
             <span className="text-6xl font-bold flex items-center">
               {counter.start}
               {counter.id === 'counter4' && <span className="text-5xl">%</span>}
