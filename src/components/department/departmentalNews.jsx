30ec5d8af260869df8764f4da9034b75d6f0e319
NabajitBhadury <nabajitbhadury@gmail.com>
2024-05-01 15:01:07 +0530

    department section initialized

---
+++ src/components/department/departmentalNews.jsx
@@ -0,0 +1,14 @@
+import 'tailwindcss/tailwind.css'
+
+const DepartmentalNews = () => {
+    return (
+        <div className="flex justify-between" style={{ height: "60vh" }}>
+            <div className="mr-auto">
+                <h1>Departmental News</h1>
+            </div>
+            <div className="ml-auto"></div>
+        </div>
+    )
+}
+
+export default DepartmentalNews
---
+++ src/components/department/imageComponent.jsx
@@ -0,0 +1,8 @@
+const ImageComponent = () => {
+    return (
+        <div class="bg-[url(/assets/aot12.png)] h-[70vh] bg-cover p-10">
+        </div>
+    )
+}
+
+export default ImageComponent
