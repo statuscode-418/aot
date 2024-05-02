918f62f0cdb910243ba41877287e480168a08b71
NabajitBhadury <nabajitbhadury@gmail.com>
2024-05-02 10:42:25 +0530

    trash

--- src/constants/companies.js
+++ src/constants/companies.js
@@ -2,7 +2,7 @@ export const companies = [
     { "img_source": "assets/tcs.png", "href": "https://www.tcs.com/" },
     { "img_source": "assets/google.png", "href": "https://www.google.co.in/" },
     { "img_source": "assets/cognizant.png", "href": "https://www.cognizant.com/in/en" },
-    { "img_source": "src/constants/amazon.png", "href": "https://www.amazon.in/" },
+    { "img_source": "assets/amazon.png", "href": "https://www.amazon.in/" },
     { "img_source": "assets/wipro.png", "href": "https://www.wipro.com/en-IN/" },
     { "img_source": "assets/capgemni.png", "href": "https://www.capgemini.com/" },
     { "img_source": "assets/hcl.png", "href": "https://www.hcltech.com/" },
