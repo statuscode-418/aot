a8c2d606b9d13c061643954a71888d4ab18d3b49
Ritam Majumder <ritmmajumder0@gmail.com>
2024-05-03 23:48:53 +0530

    bkl merge conflict

--- src/components/department/departmentalNews.jsx
+++ src/components/department/departmentalNews.jsx
@@ -3,10 +3,10 @@ import { DepartmentNews } from "../../constants/departmentNews";
 
 const DepartmentalNews = () => {
     return (
-        <div className="flex justify-between text-blue-800 font-bold " style={{ height: "65vh" }}>
+        <div className="flex justify-between text-blue-800 font-bold " >
             <div className="hidden md:block sm:block mr-auto ml-16 ">
                 <h1 className='text-5xl mb-5'>Departmental News</h1>
-                <div className='border border-black  rounded-3xl bg-white ' style={{ height: '52vh', width: '60vh' }} >
+                <div className='border border-black  rounded-3xl bg-white ' style={{ height: '65%', width: '95%' }} >
                     <img className='m-5' style={{ width: 'calc(100% - 40px)' }} src="/assets/asset1.png" alt="asset" />
 
                     <div >
@@ -25,8 +25,8 @@ const DepartmentalNews = () => {
                     </div>
                 </div>
             </div>
-            <div className="hidden sm:block md:block mx-5 self-center h-full mb-4" style={{ borderLeft: '2px solid black' }}></div>
-            <div className="ml-auto mt-7 mr-16 border rounded-3xl bg-white overflow-auto scrollbar-hide" style={{ height: '58vh', width: '60vh' }}>
+            <div className="hidden sm:block md:block mx-5 self-center h-full mb-4" style={{ borderLeft: '2px solid black',height: "65vh" }}></div>
+            <div className="hidden lg:block md:block ml-auto mt-7 mr-16 border rounded-3xl bg-white overflow-auto scrollbar-hide" style={{ height: '58vh', width: '60vh' }}>
                 {DepartmentNews.map((item, index) => (
                     <div key={index} className="p-4 mx-5 border-b-2 border-blue-800">
                         <h2 className="font-bold">{item.news}</h2>
