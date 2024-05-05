0f1820730d880b50e6fddacc22c36efd44873a33
Ritam Majumder <ritmmajumder0@gmail.com>
2024-05-06 01:11:15 +0530

    event responsive

--- src/components/department/eventScedule.jsx
+++ src/components/department/eventScedule.jsx
@@ -45,11 +45,11 @@ const eventScedule = () => {
     <div className='flex flex-col items-center justify-center'>
     <h1  className='text-[#0F2167] font-bold text-[50px] flex items-center justify-center'  >Event Scedule</h1>
     {Event_scedule.map((event, index) => (
-        <div key={index} className='flex p-2 gap-6 w-[100vw] text-white items-center justify-center'>
-            <div  className={`left border border-black h-[150px] w-[400px] bg-[#0F2167] p-4 mt-[-40px] ${event.number % 2 === 0 ? 'hidden' : ''}`}>
+        <div key={index} className='flex p-2 gap-2 lg:gap-6 text-white items-center justify-center w-[90%]'>
+            <div  className={`left  h-auto lg:h-[150px] w-[400px] bg-[#0F2167] p-4 mt-[-120px] lg:mt-[-40px] ${event.number % 2 === 0 ? 'hidden' : ''}`}>
                 {event.text}
             </div>
-            <div className={`left  h-[250px] w-[400px]  ${event.number % 2 === 1 ? 'hidden' : ''}`}>
+            <div className={`left  h-[250px] w-[400px] p-4 ${event.number % 2 === 1 ? 'hidden' : ''}`}>
                 
             </div>
 
@@ -57,14 +57,14 @@ const eventScedule = () => {
                 <div className="middle_round border border-[#Ffcc00] bg-[#0F2167] border-[5px] h-[90px] w-[90px] rounded-[50%]">
 
                 </div>
-                <div className="middle_stick border border-[#0F2167] border-[6px] h-[200px] w-[0px]">
+                <div className="middle_stick border border-[#0F2167] border-[6px] h-[400px] lg:h-[200px] w-[0px]">
 
                 </div>
             </div> 
-            <div  className={`left border border-black h-[150px] w-[400px] bg-[#0F2167] p-4 mt-[-40px] ${event.number % 2 === 1 ? 'hidden' : ''}`}>
+            <div  className={`left border border-black h-auto lg:h-[150px] w-[400px] bg-[#0F2167] p-4 mt-[-120px] lg:mt-[-40px] ${event.number % 2 === 1 ? 'hidden' : ''}`}>
                 {event.text}
             </div>
-            <div className={`left  h-[250px] w-[400px]  ${event.number % 2 === 0 ? 'hidden' : ''}`}>
+            <div className={`left  h-[250px] w-[400px] p-4 ${event.number % 2 === 0 ? 'hidden' : ''}`}>
                 
             </div>
         </div>
