01edb6317920a7d68a47e33cab39a7831cd0336a
NabajitBhadury <nabajitbhadury@gmail.com>
2024-05-05 07:05:44 +0530

    archives button taken outside

--- src/components/department/departmentalNews.jsx
+++ src/components/department/departmentalNews.jsx
@@ -3,50 +3,54 @@ import { DepartmentNews } from "../../constants/departmentNews";
 
 const DepartmentalNews = () => {
     return (
-        <div className="flex justify-between text-blue-800 font-bold p-2 " >
-            <div className=" mr-auto ml-16 ">
-                <h1 className='text-5xl mb-5'>Departmental News</h1>
-                <div className='border border-black  rounded-3xl bg-white ' style={{ height: '65%', width: '95%' }} >
-                    <img className='m-5' style={{ width: 'calc(100% - 40px)' }} src="/assets/asset1.png" alt="asset" />
-
-                    <div >
-                        <p className='ml-5'>24th April 2024</p>
-                        <p className='ml-5 text-4xl'>Lorem ipsum dolor sit, !</p>
-                        <div className='flex justify-center mt-10'>
-                            <a
-                                href="#"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="bg-blue-800 text-white px-4 border-2 border-black py-2 rounded-md"
-                            >
-                                Read more...
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className="hidden sm:block md:block mx-5 self-center h-full mb-4" style={{ borderLeft: '2px solid black',height: "65vh" }}></div>
-            <div className="hidden lg:block md:block ml-auto mt-7 mr-16 border rounded-3xl bg-white overflow-auto scrollbar-hide" style={{ height: '58vh', width: '60vh' }}>
-                {DepartmentNews.map((item, index) => (
-                    <div key={index} className="p-4 mx-5 border-b-2 border-blue-800">
-                        <h2 className="font-bold">{item.news}</h2>
-                        <p>{item.date}</p>
-
-                    </div>
-
-                ))}
-                <div className='flex justify-center '>
+        <div className="flex justify-center text-blue-800 font-bold p-2 ">
+    <div className="mr-auto ml-16 ">
+        <h1 className='text-5xl mb-5'>Departmental News</h1>
+        <div className='border border-black rounded-3xl bg-white' style={{ height: '75%', width: '95%' }}>
+            <img className='m-5' style={{ width: 'calc(100% - 40px)' }} src="/assets/asset1.png" alt="asset" />
+            <div>
+                <p className='ml-5'>24th April 2024</p>
+                <p className='ml-5 text-4xl'>Lorem ipsum dolor sit, !</p>
+                <div className='flex justify-center mt-10'>
                     <a
                         href="#"
                         target="_blank"
                         rel="noopener noreferrer"
                         className="bg-blue-800 text-white px-4 border-2 border-black py-2 rounded-md"
                     >
-                        Archives
+                        Read more...
                     </a>
                 </div>
             </div>
         </div>
+    </div>
+
+    <div className="flex items-center">
+        <div className="hidden sm:block md:block h-full" style={{ borderLeft: '2px solid black', height: "65vh" }}></div>
+    </div>
+
+    <div className="flex flex-col items-center">
+        <div className="hidden lg:block md:block ml-auto mt-7 mr-16 border rounded-3xl bg-white overflow-auto scrollbar-hide" style={{ height: '58vh', width: '60vh' }}>
+            {DepartmentNews.map((item, index) => (
+                <div key={index} className="p-4 mx-5 border-b-2 border-blue-800">
+                    <h2 className="font-bold">{item.news}</h2>
+                    <p>{item.date}</p>
+                </div>
+            ))}
+        </div>
+        <div className='flex justify-center mt-6'>
+            <a
+                href="#"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="bg-blue-800 text-white px-4 border-2 border-black py-2 rounded-md"
+            >
+                Archives
+            </a>
+        </div>
+    </div>
+</div>
+
     )
 }
 
