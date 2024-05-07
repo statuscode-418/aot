ff6beee89aac0c068c3d70ef6c757f9215832d31
Rohan-450 <zombiecpp106@gmail.com>
2024-05-08 02:19:01 +0530

    small changes

--- src/components/faculty/imageComponent.jsx
+++ src/components/faculty/imageComponent.jsx
@@ -1,5 +1,5 @@
 const ImageComponent = () => {
-	return <div class="bg-[url(/assets/faculty.jpg)] h-[80vh] bg-cover p-10"></div>;
+	return <div class="bg-[url(/assets/faculty_upd.jpg)] h-[70vh] bg-cover p-10"></div>;
 };
 
 export default ImageComponent;
