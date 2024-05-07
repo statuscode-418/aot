de41269373b043da4efe3a0927cef5af3bd6299b
NabajitBhadury <nabajitbhadury@gmail.com>
2024-05-07 21:05:38 +0530

    testing

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -33,7 +33,7 @@ const Header = () => {
 			<div className="container mx-auto flex justify-between items-center py-4">
 				<a href="/" className="flex items-center">
 					<img
-						src="/assets/aot-logo.jpg"
+						src="https://res.cloudinary.com/dtwrlsxun/image/upload/v1715096052/qwdlkowiedbklxnaqmol.jpg"
 						alt="Academy of Technology"
 						className="h-15 mr-2"
 					/>
