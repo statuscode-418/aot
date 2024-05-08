67c6defa20707d8b1e6cc0ba1696d065e8a4b717
Ritam Majumder <ritmmajumder0@gmail.com>
2024-05-08 23:34:49 +0530

    faculty page responsive

--- src/components/faculty/facultyList.jsx
+++ src/components/faculty/facultyList.jsx
@@ -8,16 +8,16 @@ const FacultyListComponent = () => {
             <table className="w-11/12 mx-auto">
                 <thead className="justify-between">
                     <tr className="bg-blue-900">
-                        <th className="px-16 py-2 border-r border-gray-400">
+                        <th className="lg:px-16 py-2 border-r border-gray-400">
                             <span className="text-gray-300">Name of the Faculty</span>
                         </th>
-                        <th className="px-16 py-2 border-r border-gray-400">
+                        <th className="lg:px-16 py-2 border-r border-gray-400">
                             <span className="text-gray-300">Designation</span>
                         </th>
-                        <th className="px-16 py-2 border-r border-gray-400">
+                        <th className="lg:px-16 py-2 border-r border-gray-400">
                             <span className="text-gray-300">Highest Qualification</span>
                         </th>
-                        <th className="px-16 py-2 border-r border-gray-400">
+                        <th className="lg:px-16 py-2 border-r border-gray-400">
                             <span className="text-gray-300">Experience</span>
                         </th>
                     </tr>
@@ -25,10 +25,10 @@ const FacultyListComponent = () => {
                 <tbody className="bg-gray-200 font-bold">
                     {faculty.map((facultyMember, index) => (
                         <tr className="bg-white border-4 border-gray-200" key={index}>
-                            <td className="px-16 py-2 text-center">{facultyMember.name}</td>
-                            <td className="px-16 py-2 text-center">{facultyMember.designation}</td>
-                            <td className="px-16 py-2 text-center">{facultyMember.qualification}</td>
-                            <td className="px-16 py-2 text-center">{facultyMember.experience}</td>
+                            <td className="lg:px-16 py-2 text-center">{facultyMember.name}</td>
+                            <td className="lg:px-16 py-2 text-center">{facultyMember.designation}</td>
+                            <td className="lg:px-16 py-2 text-center">{facultyMember.qualification}</td>
+                            <td className="lg:px-16 py-2 text-center">{facultyMember.experience}</td>
                         </tr>
                     ))}
                 </tbody>
--- src/components/faculty/imageComponent.jsx
+++ src/components/faculty/imageComponent.jsx
@@ -1,5 +1,5 @@
 const ImageComponent = () => {
-	return <div class="bg-[url(/assets/faculty_upd.jpg)] h-[70vh] bg-cover p-10"></div>;
+	return <div class="bg-[url(/assets/faculty_upd.jpg)] h-[20vh] lg:h-[70vh] w-[100%] bg-cover p-10 border border-black"></div>;
 };
 
 export default ImageComponent;
