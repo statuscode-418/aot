e89327dfa1dce757657189810f39f4750dee66a3
NabajitBhadury <nabajitbhadury@gmail.com>
2024-05-08 08:45:05 +0530

    counter updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,121 +1,156 @@
 import React, { useEffect, useRef, useState } from "react";
-import { motion, useAnimation } from "framer-motion";
 import { gsap } from "gsap";
 
 const Counter = () => {
-	const [counter, setCounter] = useState([
-		{
-			id: "counter1",
-			start: 0,
-			end: 50,
-			prefix: "",
-			suffix: "Professional Teachers",
-		},
-		{
-			id: "counter2",
-			start: 0,
-			end: 3918,
-			prefix: "",
-			suffix: "Registered Students",
-		},
-		{
-			id: "counter3",
-			start: 0,
-			end: 15198,
-			prefix: "",
-			suffix: "Established Alumni",
-		},
-		{
-			id: "counter4",
-			start: 0,
-			end: 123,
-			prefix: "",
-			suffix: "Placement on 2024",
-		},
-	]);
+  const [counter, setCounter] = useState([
+    {
+      id: "counter1",
+      end: 132,
+      prefix: "",
+      suffix: "Professional Teachers",
+      start: Math.floor(Math.random() * 45) + 1, 
+    },
+    {
+      id: "counter2",
+      start: Math.floor(Math.random() * 1000) + 1, 
+      end: 3917,
+      prefix: "",
+      suffix: "Registered Students",
+    },
+    {
+      id: "counter3",
+      start: Math.floor(Math.random() * 1000) + 1, 
+      end: 15197,
+      prefix: "",
+      suffix: "Established Alumni",
+    },
+    {
+      id: "counter4",
+      start: Math.floor(Math.random() * 45) + 1, 
+      end: 121,
+      prefix: "",
+      suffix: "Placement on 2024",
+    },
+  ]);
 
-	const [isVisible, setIsVisible] = useState(false);
-	const counterRef = useRef(null);
-	const controls = useAnimation();
+  const counterRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
 
-	useEffect(() => {
-		const observer = new IntersectionObserver(
-			(entries) => {
-				entries.forEach((entry) => {
-					if (entry.isIntersecting) {
-						setIsVisible(true);
-						startCounter();
-					} else {
-						setIsVisible(false);
-					}
-				});
-			},
-			{ threshold: 0.5 },
-		);
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsVisible(true);
+            startCounter();
+          } else {
+            setIsVisible(false);
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
 
-		if (counterRef.current) {
-			observer.observe(counterRef.current);
-		}
+    if (counterRef.current) {
+      observer.observe(counterRef.current);
+    }
 
-		return () => {
-			if (counterRef.current) {
-				observer.unobserve(counterRef.current);
-			}
-		};
-	}, []);
+    return () => {
+      if (counterRef.current) {
+        observer.unobserve(counterRef.current);
+      }
+    };
+  }, []);
 
-	const startCounter = () => {
-		const duration = 3; // Duration in seconds
-		const interval = 0.03; // Interval in seconds
+  useEffect(() => {
+    const resetCounter = () => {
+      if (!isVisible) {
+        setCounter((prevCounter) =>
+          prevCounter.map((c) => ({
+            ...c,
+            start: Math.floor(Math.random() * 1000) + 1, // Reset to random number between 1 to 1000
+          }))
+        );
+      }
+    };
 
-		counter.forEach((counterItem) => {
-			const { id, start, end } = counterItem;
-			const increment = (end - start) / (duration / interval);
-			let currentCount = start;
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (!entry.isIntersecting) {
+            resetCounter();
+          }
+        });
+      },
+      { threshold: 0.5 }
+    );
 
-			gsap.to(counterItem, {
-				start: end,
-				duration: duration,
-				ease: "power2.out",
-				onUpdate: () => {
-					setCounter((prevCounter) =>
-						prevCounter.map((c) =>
-							c.id === id ? { ...c, start: Math.ceil(currentCount) } : c,
-						),
-					);
-					currentCount += increment;
-				},
-			});
-		});
-	};
+    if (counterRef.current) {
+      observer.observe(counterRef.current);
+    }
 
-	return (
-		<div className="relative flex flex-col items-center justify-center h-[600px] bg-cover bg-bottom bg-[url('/assets/aot3.png')]">
-			<div className="absolute inset-0 bg-black opacity-40"></div>
-			<div
-				className="absolute top-1/2 left-1/2 sm:top-1/2 sm:left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row justify-center items-center space-y-8 lg:space-y-0 lg:space-x-8"
-				ref={counterRef}
-			>
-				{counter.map((counter, key) => (
-					<div className="text-white flex items-center" key={counter.id}>
-						<span className="text-6xl font-bold flex items-center">
-							{counter.start}
-							{counter.id === "counter4" && <span className="text-5xl">%</span>}
-						</span>
-						<div className="ml-2">
-							<div className="text-3xl">{counter.prefix}</div>
-							{counter.id === "counter4" && (
-								<div className="text-3xl">{counter.suffix}</div>
-							)}
-							{counter.id !== "counter4" && (
-								<div className="text-3xl">{counter.suffix}</div>
-							)}
-						</div>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+    return () => {
+      if (counterRef.current) {
+        observer.unobserve(counterRef.current);
+      }
+    };
+  }, [isVisible]);
+
+  const startCounter = () => {
+    const duration = 3; // Duration in seconds
+    const interval = 0.03; // Interval in seconds
+
+    counter.forEach((counterItem) => {
+      const { id, start, end } = counterItem;
+      const increment = (end - start) / (duration / interval);
+      let currentCount = start;
+
+      gsap.to(counterItem, {
+        start: end,
+        duration: duration,
+        ease: "power2.out",
+        onUpdate: () => {
+          setCounter((prevCounter) =>
+            prevCounter.map((c) =>
+              c.id === id ? { ...c, start: Math.ceil(currentCount) } : c
+            )
+          );
+          currentCount += increment;
+          if (currentCount >= end) {
+            gsap.killTweensOf(counterItem);
+          }
+        },
+      });
+    });
+  };
+
+  return (
+    <div className="relative flex flex-col items-center justify-center h-[600px] bg-cover bg-bottom bg-[url('/assets/aot3.png')]">
+      <div className="absolute inset-0 bg-black opacity-40"></div>
+      <div
+        className="absolute top-1/2 left-1/2 sm:top-1/2 sm:left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col lg:flex-row justify-center items-center space-y-8 lg:space-y-0 lg:space-x-8"
+        ref={counterRef}
+      >
+        {counter.map((counter, key) => (
+          <div className="text-white flex items-center" key={counter.id}>
+            <span className="text-6xl font-bold flex items-center">
+              {counter.start}
+              {counter.id === "counter4" && <span className="text-5xl">%</span>}
+            </span>
+            <div className="ml-2">
+              <div className="text-3xl">{counter.prefix}</div>
+              {counter.id === "counter4" && (
+                <div className="text-3xl">{counter.suffix}</div>
+              )}
+              {counter.id !== "counter4" && (
+                <div className="text-3xl">{counter.suffix}</div>
+              )}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Counter;
